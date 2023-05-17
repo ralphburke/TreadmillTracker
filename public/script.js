aa7cd21857729a511b1195c0f4e8f7311aff0947
Ralph <rbur6440@uni.sydney.edu.au>
2023-05-17 11:56:13 +1000

    add leg row with new id

--- public/script.js
+++ public/script.js
@@ -2,24 +2,7 @@ const type = document.getElementById("legType");
 const speed = document.getElementById("legSpeed");
 const incline = document.getElementById("legIncline");
 var inclineStore=0;
-var speedstore=0;
-
-function legGrey(){
-if(type.value=="Rest"){
-    if(speed.value!=="0"){speedStore=speed.value}
-    speed.value="0";
-    if(incline.value!=="0"){inclineStore=incline.value}
-    incline.value="0";
-    speed.setAttribute('disabled', "true");
-    incline.setAttribute('disabled', "true");
-}else{
-    speed.removeAttribute('disabled');
-    incline.removeAttribute('disabled');
-    if(speed.value=="0"){speed.value=speedStore}
-    if(incline.value=="0"){incline.value=inclineStore}};
-};
-
-document.addEventListener("click", legGrey);
+var speedStore=0;
 
 const legs=[];
 
@@ -32,14 +15,14 @@ let leg={
     heart
 }
 legs.push(leg);
-};
+}
 
 document.addEventListener("submit", function(event){
     event.preventDefault();
-    let aForm = document.getElementById("legForm");     
-addLeg(aForm.elements.legType.value, aForm.elements.legSpeed.value, parseInt(aForm.elements.legTimeS.value)+aForm.elements.legTimeM.value*60, aForm.elements.legIncline.value, aForm.elements.legHeart.value);
+    let theLegForm = document.getElementById("legForm");     
+addLeg(theLegForm.elements.legType.value, theLegForm.elements.legSpeed.value, parseInt(theLegForm.elements.legTimeS.value)+theLegForm.elements.legTimeM.value*60, theLegForm.elements.legIncline.value, theLegForm.elements.legHeart.value);
 console.log(legs);
-});
+})
 
 document.getElementById("legEntryPopupClose").onclick=function(){
     document.getElementById("legEntryPopup").style.visibility="hidden";
@@ -48,3 +31,43 @@ document.getElementById("legEntryPopupClose").onclick=function(){
 document.getElementById("legEntryPopupOpen").onclick=function(){
     document.getElementById("legEntryPopup").style.visibility="visible";
 }
+
+document.getElementById("addLegButton").addEventListener("click", function(){
+    addLegRow();
+})
+
+function addLegRow(){
+    saveLegRows();
+    legRowCopy.id="legRow"+(parseInt(legRowCopy.id.slice(-1))+1);
+    console.log(legRowCopy);
+    document.getElementById("legForm").insertBefore(legRowCopy, document.getElementById("addLegButton"));
+}
+
+function saveLegRows(){
+    legRows=document.getElementsByClassName("legEntryRow");
+    legRowCopy=legRows[legRows.length-1].cloneNode(true);
+}
+
+// function legGrey(element){
+//     if(type.value=="Rest"){
+//         if(speed.value!=="0"){speedStore=speed.value}
+//         speed.value="0";
+//         if(incline.value!=="0"){inclineStore=incline.value}
+//         incline.value="0";
+//         speed.setAttribute('disabled', "true");
+//         incline.setAttribute('disabled', "true");
+//     }else{
+//         speed.removeAttribute('disabled');
+//         incline.removeAttribute('disabled');
+//         if(speed.value=="0"){speed.value=speedStore}
+//         if(incline.value=="0"){incline.value=inclineStore}};
+//     };
+    
+//         document.getElementById("legType").addEventListener("change", event=>{
+//             saveLegRows();
+//             legRowsArray=Array.from(legRows);
+//             console.log(legRowsArray);
+//             legRowsArray.forEach(element => {
+//             legGrey(element);
+//         })
+//     })

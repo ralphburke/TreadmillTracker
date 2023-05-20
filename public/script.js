fd808ffc6885d9b114da851781e1c61a0a9b338a
Ralph <rbur6440@uni.sydney.edu.au>
2023-05-21 07:56:36 +1000

    created function for graphing session information

--- public/script.js
+++ public/script.js
@@ -38,17 +38,21 @@ function legGrey(typeSelect){
     let speed=legRow.children[2];
     let incline=legRow.children[4];
     if(typeSelect.value=="Rest"){
-        if(speed.value!=="0"){speedStore=speed.value}
-        speed.value="0";
-        if(incline.value!=="0"){inclineStore=incline.value}
-        incline.value="0";
+        // if(speed.value!=="0"){speedStore=speed.value}
+        // speed.value="0";
+        // if(incline.value!=="0"){inclineStore=incline.value}
+        // incline.value="0";
+        speed.value=null;
+        incline.value=null;
         speed.setAttribute('disabled', "true");
         incline.setAttribute('disabled', "true");
     }else{
         speed.removeAttribute('disabled');
         incline.removeAttribute('disabled');
-        if(speed.value=="0"){speed.value=speedStore}
-        if(incline.value=="0"){incline.value=inclineStore}};
+        // if(speed.value=="0"){speed.value=speedStore}
+        // if(incline.value=="0"){incline.value=inclineStore};
+        if(speed.value=="0"){speed.value=null}
+        if(incline.value=="0"){incline.value=null}};
     }
 
 function deleteLegRow(deleteButton){
@@ -66,16 +70,27 @@ legRows.forEach(element=>{
     addLeg(element);
 })
 let tiredness=undefined;
-document.getElementsByName("tiredness").forEach(element=>{if(element.checked==true){tiredness=element.value}})
+document.getElementsByName("tiredness").forEach(element=>{if(element.checked==true){tiredness=element.value}});
+
+let category=document.getElementById("sessionCategory").value;
+if(category=="Sprints"){image=""}else{
+if(category=="Jog"){image=""}else{
+if(category=="Endurance"){image=""}else{
+if(category=="Walk"){image=""}}}}
+
 let session={
-    category: document.getElementById("sessionCategory").value,
+    name: document.getElementById("nameInput").value,
+    category,
     tiredness,
     calories: document.getElementById("calories").value,
-    legs
+    legs,
+    date: new Date().toISOString(),
+    id: Date.now(),
+    image,
 }
 sessionList.push(session);
 burySessions();
-document.getElementById("SessionForm").reset();
+document.getElementById("sessionForm").reset();
     document.querySelectorAll(".legEntryRow").forEach(element=>{
     if(element.id!=="legRow0"){element.remove()}}
 )
@@ -83,12 +98,14 @@ addLegRow();
 }
 
 function addLeg(element){
+    console.log(element.querySelector(".legSpeed").value);
     let leg={
     type: element.querySelector(".typeInput").value,
-    time: element.querySelector(".legTimeM").value*60+parseInt(element.querySelector(".legTimeS").value),
-    speed: element.querySelector(".legSpeed").value,
+    time: element.querySelector(".legTimeM").value*60+parseInt(element.querySelector(".legTimeS").value+0),
+    speed: element.querySelector(".legSpeed").value+0,
     incline: element.querySelector(".legIncline").value,
-    heart: element.querySelector(".legHeart").value
+    heart: element.querySelector(".legHeart").value,
+    number: element.id.slice(6)
     }
     legs.push(leg);
 }
@@ -100,4 +117,52 @@ function burySessions(){
 function resurrectSessions(){
     localStorage.getItem("sessionList")===null?sessionList=[]
     :sessionList=JSON.parse(localStorage.getItem("sessionList"));
+}
+
+function constructGraph(sessionNum){
+    let session=sessionList[sessionNum];
+    let totalTime=0;
+    let biggestSpeed=0;
+    session.legs.forEach(leg=>{
+        if(leg.speed>parseInt(biggestSpeed)){biggestSpeed=leg.speed};
+        totalTime+=leg.time;
+        let newBar=document.createElement("div");
+        newBar.setAttribute("data-time", leg.time);
+        newBar.setAttribute("data-speed", leg.speed);
+        newBar.setAttribute("class", "bar");
+        newBar.setAttribute("id", leg.number);
+        if(leg.type=="Sprint"){        var colour="orange"
+        }else{if(leg.type="Jog"){  var colour="green"
+            }else{                  var colour="blue"
+            }
+        }
+        newBar.setAttribute("data-colour",colour);
+        graphLocation=document.getElementById("graph1");
+        graphLocation.appendChild(newBar);
+    })
+    let barWidth=500;
+    let barHeight=100;
+    graphLocation.querySelectorAll(".bar").forEach(bar=>{
+        bar.style.width=(barWidth*bar.getAttribute("data-time")/totalTime)+"px";
+        bar.style.height=(barHeight*bar.getAttribute("data-speed")/biggestSpeed)+"px";
+        bar.style.backgroundColor=bar.getAttribute("data-colour");
+    })
+}
+
+
+document.getElementById("writeSessions").addEventListener("click", logger);
+function logger(){
+    console.log(sessionList);
+
+    // document.querySelectorAll(".bar").forEach(bar=>{
+    //     console.log(bar.style.width);
+    //     bar.style.width=bar.style.width.slice(0,-2)*0.5+"px";
+    //     console.log(bar.style.width);
+    // })
+}
+
+document.getElementById("make").addEventListener("click", constructor);
+
+function constructor(){
+    constructGraph(sessionList.length-1);
 }

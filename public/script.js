6cfd38e5c1ed4cbb2bb419b6856d5a79b2ef542d
Ralph <rbur6440@uni.sydney.edu.au>
2023-05-21 17:17:33 +1000

    fixed required inputs and past sessions display

--- public/script.js
+++ public/script.js
@@ -1,14 +1,12 @@
-// var inclineStore=0;
-// var speedStore=0;
 resurrectSessions();
 addLegRow();
 
-document.getElementById("legEntryPopupClose").onclick=function(){
-    document.getElementById("legEntryPopup").style.visibility="hidden";
+document.getElementById("sessionEntryPopupClose").onclick=function(){
+    document.getElementById("sessionEntryPopup").style.visibility="hidden";
 }
 
-document.getElementById("legEntryPopupOpen").onclick=function(){
-    document.getElementById("legEntryPopup").style.visibility="visible";
+document.getElementById("sessionEntryPopupOpen").onclick=function(){
+    document.getElementById("sessionEntryPopup").style.visibility="visible";
 }
 
 document.getElementById("addLegButton").addEventListener("click", function(){
@@ -18,14 +16,17 @@ document.getElementById("addLegButton").addEventListener("click", function(){
 function addLegRow(){
     blankRow=document.getElementById("legRow0");
     legRowCopy=blankRow.cloneNode(true);
-    rowNum=document.getElementsByClassName("legEntryRow").length;
-    legRowCopy.id="legRow"+(parseInt(document.getElementsByClassName("legEntryRow")[rowNum-1].id.slice(6))+1);
+    // console.log(document.getElementsByClassName("legEntryRow").length);
+    let rowNum=document.getElementsByClassName("legEntryRow").length;
+    console.log(rowNum);
+    rowNum==1?legRowCopy.id="legRow1":legRowCopy.removeAttribute("id");
+    // legRowCopy.id="legRow"+(parseInt(document.getElementsByClassName("legEntryRow")[rowNum-1].id.slice(6))+1);
     if(legRowCopy.id==="legRow1"){legRowCopy.children[8].style.visibility="hidden"};
     document.getElementById("legForm").insertBefore(legRowCopy, document.getElementById("addLegButton"));
-    document.getElementsByClassName("typeInput")[rowNum].addEventListener("change", event=>{
+    document.getElementsByClassName("typeInput")[rowNum-1].addEventListener("change", event=>{
         legGrey(event.target);
     })
-    document.getElementsByClassName("legRowDelete")[rowNum].addEventListener("click", event=>{
+    document.getElementsByClassName("legRowDelete")[rowNum-1].addEventListener("click", event=>{
         deleteLegRow(event.target);
     })
 }
@@ -35,10 +36,6 @@ function legGrey(typeSelect){
     let speed=legRow.children[2];
     let incline=legRow.children[4];
     if(typeSelect.value=="Rest"){
-        // if(speed.value!=="0"){speedStore=speed.value}
-        // speed.value="0";
-        // if(incline.value!=="0"){inclineStore=incline.value}
-        // incline.value="0";
         speed.value=null;
         incline.value=null;
         speed.setAttribute('disabled', "true");
@@ -46,21 +43,17 @@ function legGrey(typeSelect){
     }else{
         speed.removeAttribute('disabled');
         incline.removeAttribute('disabled');
-        // if(speed.value=="0"){speed.value=speedStore}
-        // if(incline.value=="0"){incline.value=inclineStore};
-        if(speed.value=="0"){speed.value=null}
-        if(incline.value=="0"){incline.value=null}};
+    }
     }
 
 function deleteLegRow(deleteButton){
         deleteButton.parentNode.remove();
 }
 
-document.getElementById("addSession").addEventListener("click", addSession)
+document.getElementById("sessionEntryPopup").addEventListener("submit", addSession);
 
 function addSession(){
 let legRows=Array.from(document.getElementById("legForm").children);
-legRows.shift();
 legRows.pop();
 legs=[];
 legRows.forEach(element=>{
@@ -94,10 +87,10 @@ document.getElementById("sessionForm").reset();
     if(element.id!=="legRow0"){element.remove()}}
 )
 addLegRow();
+pastSessionConstruct(sessionList[sessionList.length-1]);
 }
 
 function addLeg(element){
-    // console.log(element.querySelector(".legSpeed").value);
     let leg={
     type: element.querySelector(".typeInput").value,
     time: element.querySelector(".legTimeM").value*60+parseInt(element.querySelector(".legTimeS").value||0),
@@ -113,8 +106,26 @@ function burySessions(){
 }
 
 function resurrectSessions(){
-    localStorage.getItem("sessionList")===null?sessionList=[]
-    :sessionList=JSON.parse(localStorage.getItem("sessionList"));
+    localStorage.getItem("sessionList")===null?sessionList=[]:sessionList=JSON.parse(localStorage.getItem("sessionList"));
+}
+
+function writeSessions(){
+    sessionList.forEach(session=>pastSessionConstruct(session));
+}
+
+function pastSessionConstruct(session){
+    blankSession=document.getElementById("pastSession0");
+    pastSessionCopy=blankSession.cloneNode(true);
+    pastSessionCopy.removeAttribute("id");
+    pastSessionCopy.querySelector(".sessionName").innerHTML=session.name;
+    constructGraph(session, pastSessionCopy.querySelector(".graph"));
+    pastSessionCopy.querySelector(".distanceStat").innerHTML=((session.totalTime/3600)*session.totalSpeed/session.legs.length).toFixed(2)+"<span class=\"sessionUnits\"> km/h</span>";
+    pastSessionCopy.querySelector(".timeStatM").innerHTML=Math.floor(session.totalTime/60);
+    pastSessionCopy.querySelector(".timeStatS").innerHTML=session.totalTime%60;
+    if(session.calories!==""){pastSessionCopy.querySelector(".calorieStat").innerHTML=session.calories+"<span class=\"sessionUnits\"> kcal</span>"}else{
+        pastSessionCopy.querySelector(".calorieStat").remove();
+    };
+    document.getElementById("sessionsMainPage").appendChild(pastSessionCopy);
 }
 
 function constructGraph(session, graphLocation){
@@ -144,42 +155,11 @@ function constructGraph(session, graphLocation){
     })
 }
 
-
-document.getElementById("writeSessions").addEventListener("click", logger);
-function logger(){
-    // console.log(sessionList);
-    pastSessionsMainPage();
-    // document.querySelectorAll(".bar").forEach(bar=>{
-    //     console.log(bar.style.width);
-    //     bar.style.width=bar.style.width.slice(0,-2)*0.5+"px";
-    //     console.log(bar.style.width);
-    // })
-}
-
-// document.getElementById("make").addEventListener("click", constructor);
-
-// function constructor(){
-//     pastSessionConstruct(sessionList[sessionList.length-1]);
-// }
-
-function pastSessionConstruct(session){
-    document.getElementById("sessionsMainPage").querySelectorAll(".pastSession").forEach(pastSession=>{
-        if(pastSession.id!=="pastSession0"){pastSession.remove()}
+document.getElementById("writeSessions").addEventListener("click", function(){
+    // writeSessions();
+    document.querySelectorAll("input,select").forEach(element=>{
+        if(element.hasAttribute("required")){
+            if(element.value==""){console.log("asaadsfa")}
+        }
     })
-    blankSession=document.getElementById("pastSession0");
-    pastSessionCopy=blankSession.cloneNode(true);
-    pastSessionCopy.removeAttribute("id");
-    pastSessionCopy.querySelector(".sessionName").innerHTML=session.name;
-    constructGraph(session, pastSessionCopy.querySelector(".graph"));
-    pastSessionCopy.querySelector(".distanceStat").innerHTML=((session.totalTime/3600)*session.totalSpeed/session.legs.length).toFixed(2)+"<span class=\"sessionUnits\"> km/h</span>";
-    pastSessionCopy.querySelector(".timeStatM").innerHTML=Math.floor(session.totalTime/60);
-    pastSessionCopy.querySelector(".timeStatS").innerHTML=session.totalTime%60;
-    if(session.calories!==""){pastSessionCopy.querySelector(".calorieStat").innerHTML=session.calories+"<span class=\"sessionUnits\"> kcal</span>"}else{
-        pastSessionCopy.querySelector(".calorieStat").remove();
-    };
-    document.getElementById("sessionsMainPage").appendChild(pastSessionCopy);
-}
-
-function pastSessionsMainPage(){
-    sessionList.forEach(session=>pastSessionConstruct(session));
-}
+})

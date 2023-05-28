4e1e7156486ad5769d57ff02d632e4e6a9ef7ba0
Ralph <rbur6440@uni.sydney.edu.au>
2023-05-28 10:06:59 +1000

    Session delete button added

--- public/script.js
+++ public/script.js
@@ -1,4 +1,4 @@
-resurrectSessions();
+loadSessions();
 writeSessions();
 addLegRow();
 
@@ -14,7 +14,7 @@ function closeSessionEntry(){
 
 document.getElementById("sessionEntryPopupOpen").addEventListener("click",function(){
     sessionReset();
-    resurrectTemplates();
+    loadTemplates();
     listTemplates();
     moveToHistory();
     document.getElementById("sessionEntryPopup").style.visibility="visible";
@@ -29,13 +29,13 @@ function addLegRow(){
     legRowCopy=blankRow.cloneNode(true);
     let rowNum=document.getElementsByClassName("legEntryRow").length;
     rowNum==1?legRowCopy.id="legRow1":legRowCopy.removeAttribute("id");
-    if(legRowCopy.id==="legRow1"){legRowCopy.children[8].style.visibility="hidden"};
+    if(legRowCopy.id==="legRow1"){legRowCopy.querySelector(".legRowDelete").style.visibility="hidden"};
     document.getElementById("legForm").insertBefore(legRowCopy, document.getElementById("addLegButton"));
     document.getElementsByClassName("typeInput")[rowNum-1].addEventListener("change", event=>{
         legGrey(event.target);
     })
     document.getElementsByClassName("legRowDelete")[rowNum-1].addEventListener("click", event=>{
-        deleteLegRow(event.target);
+        event.target.parentNode.remove();
     })
 }
 
@@ -46,17 +46,13 @@ function legGrey(typeSelect){
     if(typeSelect.value=="Rest"){
         speed.value=null;
         incline.value=null;
-        speed.setAttribute('disabled', "true");
-        incline.setAttribute('disabled', "true");
+        speed.setAttribute("disabled", "true");
+        incline.setAttribute("disabled", "true");
     }else{
-        speed.removeAttribute('disabled');
-        incline.removeAttribute('disabled');
+        speed.removeAttribute("disabled");
+        incline.removeAttribute("disabled");
     }
     }
-
-function deleteLegRow(deleteButton){
-        deleteButton.parentNode.remove();
-}
 
 document.getElementById("sessionEntryPopup").addEventListener("submit", event=>{
     event.preventDefault();
@@ -86,14 +82,14 @@ let session={
     tiredness,
     calories: document.getElementById("calories").value,
     legs,
-    date: new Date().toISOString(),
+    date: new Date().toLocaleString("en-AU"),
     id: Date.now(),
     image,
     totalSpeed:0,
     totalTime:0
 }
 sessionList.push(session);
-burySessions();
+saveSessions();
 
 if(document.getElementById("templateYes").checked==1){
     let template={
@@ -104,7 +100,7 @@ if(document.getElementById("templateYes").checked==1){
         legs,
     }
     templateList.push(template);
-    buryTemplates();
+    saveTemplates();
 }
 sessionReset();
 pastSessionConstruct(sessionList[sessionList.length-1]);
@@ -130,19 +126,19 @@ function addLeg(element){
     legs.push(leg);
 }
 
-function burySessions(){
+function saveSessions(){
     localStorage.setItem("sessionList", JSON.stringify(sessionList));
 }
 
-function resurrectSessions(){
+function loadSessions(){
     localStorage.getItem("sessionList")===null?sessionList=[]:sessionList=JSON.parse(localStorage.getItem("sessionList"));
 }
 
-function buryTemplates(){
+function saveTemplates(){
     localStorage.setItem("templateList", JSON.stringify(templateList));
 }
 
-function resurrectTemplates(){
+function loadTemplates(){
     localStorage.getItem("templateList")===null?templateList=[]:templateList=JSON.parse(localStorage.getItem("templateList"));
 }
 
@@ -190,6 +186,9 @@ document.getElementById("sessionTemplate").addEventListener("change", event=>{
 })
 
 function writeSessions(){
+    document.querySelectorAll(".pastSession").forEach(element=>{
+        if(element.id!=="pastSession0"){element.remove()}
+    })
     sessionList.forEach(session=>pastSessionConstruct(session));
 }
 
@@ -201,15 +200,14 @@ function pastSessionConstruct(session){
     constructGraph(session, pastSessionCopy.querySelector(".graph"));
     pastSessionCopy.querySelector(".distanceStat").innerHTML=((session.totalTime/3600)*session.totalSpeed/session.legs.length).toFixed(2)+"<span class=\"sessionUnits\"> km</span>";
     pastSessionCopy.querySelector(".timeStatM").innerText=Math.floor(session.totalTime/60);
-    let timeS=(session.totalTime%60).toString();
-    if(timeS[1]==undefined){timeS="0"+timeS};
-    pastSessionCopy.querySelector(".timeStatS").innerText=timeS;
+    pastSessionCopy.querySelector(".timeStatS").innerText=(session.totalTime%60).toLocaleString("en-AU", {minimumIntegerDigits:2});
     if(session.calories!==""){pastSessionCopy.querySelector(".calorieStat").innerHTML=session.calories+"<span class=\"sessionUnits\"> kcal</span>"}else{
         pastSessionCopy.querySelector(".calorieStat").remove();
     };
     pastSessionCopy.addEventListener("click", event=>{
         let pastSession=event.target.closest(".pastSession");
         let index=Array.prototype.indexOf.call(document.querySelectorAll(".pastSession"),pastSession)-1
+        document.getElementById("deleteButton").setAttribute("data-sessionIndex", index);
         viewSession(pastSession, sessionList[index])
     });
     document.getElementById("sessionsRow").appendChild(pastSessionCopy);
@@ -255,13 +253,62 @@ function viewSession(pastSession, session){
     let leftSide=document.getElementById("sessionHistoryLeft");
     leftSide.querySelector("#historyName").innerText=session.name;
     leftSide.querySelector("#historyCategory").innerText="Category: "+session.category;
+    leftSide.querySelector("#historyDate").innerText="Added "+session.date;
+    leftSide.querySelector("#historyID").innerHTML="ID: "+session.id;
+    newHistoryRows(session);
 }
 
 function enlargeGraph(graphLocation){
     graphLocation.querySelectorAll(".bar").forEach(bar=>{
+        bar.style.width=(0.7*bar.style.width.slice(0,-2))+"px";
         bar.style.height=(2*bar.style.height.slice(0,-2))+"px";
     })
 }
+
+function newHistoryRows(session){
+    let historyRow=document.getElementsByClassName("historyRow")[0].cloneNode(true);
+    let historyLegs=document.getElementById("historyLegs");
+    historyLegs.innerHTML="";
+    let historyID=1;
+    session.legs.forEach(leg=>{
+        let newRow=historyRow.cloneNode(true);
+        newRow.querySelector(".historyType").innerText=leg.type;
+        newRow.querySelector(".historySpeed").innerHTML=leg.speed;
+        newRow.querySelector(".historyTimeM").innerText=Math.floor(leg.time/60);
+        newRow.querySelector(".historyTimeS").innerText=(leg.time%60).toLocaleString("en-AU", {minimumIntegerDigits:2});
+        newRow.id="historyRow"+historyID;
+        historyID+=1;
+        let extra1=newRow.querySelector(".historyExtra1");
+        let extra2=newRow.querySelector(".historyExtra2");
+        extra1.removeAttribute("id");
+        extra2.removeAttribute("id");
+        extra1.innerText="";
+        extra2.innerText="";
+        if(leg.incline!==""){
+            extra1.setAttribute("id","historyIncline");
+            extra1.innerHTML=leg.incline+"<span>°</span>";
+            if(leg.heart!==""){
+                extra2.setAttribute("id","historyHeart");
+                extra2.innerHTML=leg.heart+"<span class=\"historyUnits\">❤</span>";
+            }
+        }else{
+            if(leg.heart!==""){
+            extra1.setAttribute("id","historyHeart");
+            extra1.innerHTML=leg.heart+"<span class=\"historyUnits\">❤</span>";
+        }}
+        historyLegs.appendChild(newRow);
+    })
+}
+
+document.getElementById("deleteButton").addEventListener("click", function(){
+    let sessionIndex=document.getElementById("deleteButton").getAttribute("data-sessionIndex");
+    sessionList.splice(sessionIndex, 1);
+    saveSessions();
+    moveFromHistory();
+    writeSessions();
+    document.getElementById("history").style.visibility="hidden";
+    document.getElementById("sessionHistory").style.visibility="hidden";
+})
 
 document.getElementById("showFullHistory").addEventListener("click", function(){
     moveToHistory();
@@ -289,5 +336,4 @@ function moveFromHistory(){
 document.getElementById("backToFullHistory").addEventListener("click", function(){
     document.getElementById("fullHistory").style.visibility="visible";
     document.getElementById("sessionHistory").style.visibility="hidden";
-
 })
